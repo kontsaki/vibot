edb6f30937b3d5ee2dd1c062bc28a489c599db3c
Kon Tsaki <kontsaki@pm.me>
2020-12-10 13:44:05 +0100

    Handle Event `conversation_started`

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@ use warp::{reply, Filter};
 #[tokio::main]
 async fn main() {}
 
+pub fn events() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("viber" / "events").and(warp::body::json()).map(
+        |event_info: HashMap<String, String>| {
+            if let Some(event) = event_info.get("event") {
+                match event.as_str() {
+                    "conversation_started" => conversaton_started(),
+                    _ => reply::json(&json!({})),
+                }
+            } else {
+                reply::json(&json!({}))
+            }
+        },
+    )
+}
+
 fn webhook(webhook_url: &str, api_key: String, site_url: String) -> reqwest::RequestBuilder {
     let client = reqwest::Client::new();
     client
@@ -17,10 +32,62 @@ fn webhook(webhook_url: &str, api_key: String, site_url: String) -> reqwest::Req
         }))
 }
 
+fn conversaton_started() -> warp::reply::Json {
+    reply::json(&json!({
+        "type": "picture",
+        "text": "Welcome",
+        "media": "https://a-picture",
+    }))
+}
+
 #[cfg(test)]
 mod tests {
-    use super::webhook;
+    use super::{events, webhook};
     use serde_json::json;
+    use warp::http::StatusCode;
+    use warp::test::request;
+
+    #[tokio::test]
+    async fn test_events_conversation_started() {
+        let api = events();
+
+        let resp = request()
+            .method("POST")
+            .path("/viber/events")
+            .json(&json!({
+                "event": "conversation_started"
+            }))
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(
+            resp.into_body(),
+            json!({
+                "type": "picture",
+                "text": "Welcome",
+                "media": "https://a-picture"
+            })
+            .to_string()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_events_unrelated() {
+        let api = events();
+
+        let resp = request()
+            .method("POST")
+            .path("/viber/events")
+            .json(&json!({
+                "event": "unrelated"
+            }))
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(resp.into_body(), json!({}).to_string());
+    }
 
     #[test]
     fn test_webhook_request_data() {
